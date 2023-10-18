729cb29392c1e5ea55e2bf829de8a9398f269eff
sofymiranda <mirandaspfya@gmail.com>
2023-10-18 10:27:22 -0300

    atividade 13

---
+++ atv13.py
@@ -0,0 +1,16 @@
+# Exercício Python 14: Faça um programa que leia três números e
+# mostre qual é o maior e qual é o menor.
+n1=float(input("Digite o primeiro numero: "))
+n2=float(input("Digite o segundo numero: "))
+n3=float(input("Digite o terceiro numero: "))
+maior=n1
+if n2>n1 and n2.n3:
+    maior=n2
+    if n3 > n1 and n3 > n2:
+     maior = n3
+    if n2 < n3 and n2 < n1:
+        menor = n2
+        if n3 < n2 and n3 < n1:
+            menor = n3
+            print (f"o maior numero numero digitado foi {maior}")
+            print (f"o menor numero digitado foi {menor}")
